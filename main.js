0b99b9888fd970e08d35657bbdff7abf83efe881
CraigCarter <craigdotcarter@gmail.com>
2017-04-13 20:04:03 -0400

    Product Bar Updates

--- main.js
+++ main.js
@@ -7,15 +7,39 @@ var cartnumber = 0;
 
 $(document).ready(
   $('.button').click(function(){
-    $('.button').html("Added to Cart!");
+    $('.button').html('Added to Cart!');
     cartnumber++;
     $('.shoppingcart span').html(cartnumber);
 
     setTimeout(function(){
-      $('.button').html("Buy Now!");
+      $('.button').html('Buy Now!');
     }, 1000);
-  }
-));
+  }),
+  $('.productdetails').click(function(){
+    $('.productdetails').addClass('active');
+    $('.sizefit').removeClass('active');
+    $('.reviewsstars').removeClass('active');
+    $('.excitingtopic').show();
+    $('.sizefitdescription').hide();
+    $('.customersaresaying').hide();
+  }),
+  $('.sizefit').click(function(){
+    $('.sizefit').addClass('active');
+    $('.productdetails').removeClass('active');
+    $('.reviewsstars').removeClass('active');
+    $('.sizefitdescription').show();
+    $('.excitingtopic').hide();
+    $('.customersaresaying').hide();
+  }),
+  $('.reviewsstars').click(function(){
+    $('.reviewsstars').addClass('active');
+    $('.productdetails').removeClass('active');
+    $('.sizefit').removeClass('active');
+    $('.customersaresaying').show();
+    $('.excitingtopic').hide();
+    $('.sizefitdescription').hide();
+  })
+);
 
 
 /*  var original = " "
